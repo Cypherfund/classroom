a25495936b5ccd6cc36ecc106752c9ea529cf901
fiona-max <anyefiona@gmail.com>
2024-03-05 18:01:02 +0200

    completed the payments UI

--- src/environments/app.config.ts
+++ src/environments/app.config.ts
@@ -1,3 +1,7 @@
 export const appConfig = {
-  profileImage: 'assets/images/download-img.png'
+  profileImage: 'assets/images/download-img.png',
+  imageOne: 'assets/images/paypal.png',
+  imageTwo: 'assets/images/american-express.png',
+  imageThree: 'assets/images/visa.png',
+  imageFour: 'assets/images/master-card.png'
 }
