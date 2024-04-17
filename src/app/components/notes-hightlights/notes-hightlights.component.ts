9918412c97d956d65cfcc3454f8311679b7e02dc
tiwavaldese <tiwaval7@gmail.com>
2024-04-17 13:26:58 +0100

    updating the note and highlight page with some content

--- src/app/components/notes-hightlights/notes-hightlights.component.ts
+++ src/app/components/notes-hightlights/notes-hightlights.component.ts
@@ -19,14 +19,14 @@ export class NotesHightlightsComponent implements OnInit{
 
   initializeNotes():void{
 this.notes = [
-  {id:1,content:"hello world"},
-  {id:2,content:"hey world"}
+  {id:1,content:"Angular is a popular open-source framework for building dynamic web applications. Developed and maintained by Google, Angular provides a comprehensive solution for front-end development, allowing developers to create powerful single-page applications (SPAs) with ease."},
+  {id:2,content:"Angular follows a component-based architecture, where the application is composed of reusable and encapsulated components. This makes it easier to develop, maintain, and test applications."}
 ]
   }
   initializeHightlights():void{
     this.highlights = [
-      {id:1,content:"hello world"},
-      {id:2,content:"hey world"}
+      {id:1,content:"Angular is a popular open-source framework for building dynamic web applications."},
+      {id:2,content:"This makes it easier to develop."}
     ]
   }
   editNote(note:{id:number,content:string}): void{
