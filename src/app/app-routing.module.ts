6ed28a69f7fa960bfa076d0b0542b98bf854c106
tiwavaldese <tiwaval7@gmail.com>
2024-04-23 09:25:39 +0100

    linking the reminder pages to the learning reminder page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { HomepageComponent } from './pages/homepage/homepage.component';
 import {AboutCourseComponent} from "./pages/course-tab-details-page/about-course/about-course.component";
 import {ShoppingCartComponent} from "./pages/shopping-cart/shopping-cart.component";
 import {PaymentsComponent} from "./pages/payments/payments.component";
-import { TransactionComponent } from './components/transaction/transaction.component';
 import { SettingsComponent } from './settings/settings.component';
 import {CourseDetailsPageComponent} from "./pages/course-details-page/course-details-page.component";
-import { AccountComponent } from './components/account/account.component';
 import {CoursesComponent} from "./pages/courses/courses.component";
 import { CommunicationComponent } from './components/communication/communication.component';
+import { LearningRemindersComponent } from './components/learning-reminders/learning-reminders.component';
+import { ReminderPopup1Component } from './components/reminder-popup1/reminder-popup1.component';
+import { ReminderPopup2Component } from './components/reminder-popup2/reminder-popup2.component';
 
 
 const routes: Routes = [
@@ -17,16 +18,17 @@ const routes: Routes = [
   { path:'about', component:AboutCourseComponent},
   { path: 'shopping_chart', component:ShoppingCartComponent},
   { path:'payment', component:PaymentsComponent},
-  { path:'transaction', component: TransactionComponent},
   { path:'about', component: AboutCourseComponent},
   { path: 'course_detail/:id', component: CourseDetailsPageComponent},
   { path: 'settings',loadChildren: ()=> import('./settings/settings.module').then(m =>m.SettingsModule)},
   { path: 'course-admin', loadChildren: () => import('./pages/course-admin/course-admin.module').then(m => m.CourseAdminModule) },
   { path:'about', component:AboutCourseComponent},
   { path: 'settings', component:SettingsComponent},
-  { path: 'account', component:AccountComponent},
   { path: 'courses', component: CoursesComponent},
-  {path: 'communication', component: CommunicationComponent}
+  {path: 'communication', component: CommunicationComponent},
+  { path: 'learning-reminder', component: LearningRemindersComponent},
+  { path: 'reminder-popup1', component: ReminderPopup1Component},
+  { path: 'reminder-popup2', component: ReminderPopup2Component}
 ];
 
 @NgModule({
