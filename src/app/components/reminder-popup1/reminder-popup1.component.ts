bccad8e0e1f5d2c3ace876baa0f1ca1d0b3ca69b
tiwavaldese <tiwaval7@gmail.com>
2024-04-24 10:39:21 +0100

    restructure the reminder popup 1 page and update the styling

--- src/app/components/reminder-popup1/reminder-popup1.component.ts
+++ src/app/components/reminder-popup1/reminder-popup1.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-reminder-popup1',
   templateUrl: './reminder-popup1.component.html',
-  styleUrl: './reminder-popup1.component.scss'
+  styleUrl: './reminder-popup1.component.scss',
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ReminderPopup1Component {
 
