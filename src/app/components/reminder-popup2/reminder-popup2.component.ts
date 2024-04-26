9c2041b5c8726e7af3b3d55231440ee09b752518
tiwavaldese <tiwaval7@gmail.com>
2024-04-26 14:34:12 +0100

    update of the reminder popup 2 page

--- src/app/components/reminder-popup2/reminder-popup2.component.ts
+++ src/app/components/reminder-popup2/reminder-popup2.component.ts
@@ -8,6 +8,14 @@ import {FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors} f
 })
 export class ReminderPopup2Component implements OnInit {
   reminderForm: FormGroup;
+  once: boolean = true;
+  daily: boolean = false;
+  weekly: boolean = false;
+  monthly: boolean = false;
+  fiveMins:boolean = true;
+  tenMins: boolean = false;
+  thirtyMins: boolean = false;
+  oneHour: boolean = false;
   constructor(
     private fb: FormBuilder
   ){
@@ -31,5 +39,17 @@ else{
   console.log('Form is invalid');
   
 }
+  }
+  currentFrequency(reminderPopUp2:string):void{
+this.once = reminderPopUp2 === "once"
+this.daily = reminderPopUp2 === "daily"
+this.weekly = reminderPopUp2 === "weekly";
+this.monthly = reminderPopUp2 === "monthly"
+  }
+  currentTime(reminderPopUp:string):void{
+    this.fiveMins = reminderPopUp === 'fiveMins';
+  this.tenMins= reminderPopUp === 'tenMins';
+  this.thirtyMins= reminderPopUp === 'thirtyMins';
+  this.oneHour= reminderPopUp === 'oneHour';
   }
 }
