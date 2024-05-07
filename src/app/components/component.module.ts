b2abf8e07f983692fb6f34a194938856209099c4
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-05-07 22:42:58 +0100

    continue to update styles

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
+import { AvatarModule } from 'primeng/avatar';
+import { BadgeModule } from 'primeng/badge';
+import { MenubarModule } from 'primeng/menubar';
+import { RippleModule } from 'primeng/ripple';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 
 
@@ -11,7 +17,13 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     TranslateModule.forChild(),
-    RouterModule.forChild([])
+    RouterModule.forChild([]),
+    AvatarModule,
+    BadgeModule,
+    MenubarModule,
+    RippleModule,
+    InputTextModule,
+    ButtonModule
   ],
   exports: [NavbarComponent]
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {appConfig} from "../../../environments/app.config";
+import { MenuItem } from 'primeng/api';
+import { UserService } from '../../services/user/user.service';
 
 
 @Component({
@@ -8,10 +10,72 @@ import {appConfig} from "../../../environments/app.config";
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
- profileImage = appConfig.profileImage
+  items: MenuItem[] | undefined;
+  isLogged: boolean = false;
+
+  profileImage = appConfig.profileImage
+
+  constructor(private userService: UserService) {
+  }
 
   ngOnInit() {
+    this.isLogged = !!Object.keys(this.userService.user).length;
 
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home'
+      },
+      {
+        label: 'Features',
+        icon: 'pi pi-star'
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
+          {
+            label: 'Core',
+            icon: 'pi pi-bolt',
+            shortcut: '⌘+S'
+          },
+          {
+            label: 'Blocks',
+            icon: 'pi pi-server',
+            shortcut: '⌘+B'
+          },
+          {
+            label: 'UI Kit',
+            icon: 'pi pi-pencil',
+            shortcut: '⌘+U'
+          },
+          {
+            separator: true
+          },
+          {
+            label: 'Templates',
+            icon: 'pi pi-palette',
+            items: [
+              {
+                label: 'Apollo',
+                icon: 'pi pi-palette',
+                badge: '2'
+              },
+              {
+                label: 'Ultima',
+                icon: 'pi pi-palette',
+                badge: '3'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+        badge: '3'
+      }
+    ];
   }
   searchNav(){
     console.log('found course')
