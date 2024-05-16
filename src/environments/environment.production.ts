4240a2f3a9ce8d35838e3391182de41d7e9f8e21
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-05-16 07:05:22 +0100

    fix redirect

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   envName: 'production',
   apiUrl: '',
-  authUrl: 'https://auth.tech-ascend',
+  authUrl: 'https://auth.tech-ascend.com',
   CourseUrl: 'https://tech-ascend/course-api',
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   envName: 'mock',
   apiUrl: '',
-  authUrl: 'http://auth.tech-ascend.com',
+  authUrl: 'https://auth.tech-ascend.com',
   CourseUrl: 'http://161.35.249.4/course-api'
 };
