00a69b0a56201cbd5a69a6ad54c30cabda9bbc64
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-06-05 08:01:59 +0100

    fix host

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   envName: 'production',
-  apiUrl: 'https://tech-ascend',
+  apiUrl: 'https://tech-ascend.com',
   authUrl: 'https://auth.tech-ascend.com',
   CourseUrl: 'https://tech-ascend/course-api',
   imagebucket: 'assets/images/'
