e08bd506697545545f322913f87a977c48f34cdf
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-06-19 06:46:45 +0100

    fix sidebar height

--- src/app/pages/course-admin/course-admin-layout.component.ts
+++ src/app/pages/course-admin/course-admin-layout.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import { Sidebar } from 'primeng/sidebar';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-course-admin-layout',
@@ -7,5 +8,14 @@ import { Sidebar } from 'primeng/sidebar';
   styleUrl: './course-admin-layout.component.scss',
   host: {ngSkipHydration: 'true'}
 })
-export class CourseAdminLayoutComponent {
+export class CourseAdminLayoutComponent implements OnInit, OnDestroy{
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+    ngOnDestroy(): void {
+    this.document.body.classList.remove('no-scroll');
+  }
+
+  ngOnInit(): void {
+    this.document.body.classList.add('no-scroll');
+  }
 }
