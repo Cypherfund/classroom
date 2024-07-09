43d033f865af9dbbbec628186fb90cdd615c3281
E.Ngai <asobingai@gmail.com>
2024-07-09 12:35:54 +0100

    update loadbalancer

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   envName: 'development',
-  apiUrl: 'http://159.89.252.130',
-  authUrl: 'http://159.89.252.130',
-  CourseUrl: 'http://159.89.252.130/course-api',
+  apiUrl: 'http://144.126.250.16',
+  authUrl: 'http://144.126.250.16',
+  CourseUrl: 'http://144.126.250.16/course-api',
   imagebucket: 'assets/images/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   envName: 'production',
-  apiUrl: 'http://159.89.252.130',
+  apiUrl: 'http://144.126.250.16',
   authUrl: 'https://auth.tech-ascend.com',
   CourseUrl: 'https://tech-ascend.com/course-api',
   imagebucket: 'assets/images/'
