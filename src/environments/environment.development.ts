684ef19ab7373dcc7dbb4b4fff9cf2cc817e7aaf
E.Ngai <asobingai@gmail.com>
2024-07-09 12:52:44 +0100

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   envName: 'development',
-  apiUrl: 'http://144.126.250.16',
-  authUrl: 'http://144.126.250.16',
-  CourseUrl: 'http://144.126.250.16/course-api',
+  apiUrl: 'http://159.89.242.197',
+  authUrl: 'http://159.89.242.197',
+  CourseUrl: 'http://159.89.242.197/course-api',
   imagebucket: 'assets/images/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   envName: 'production',
-  apiUrl: 'http://144.126.250.16',
+  apiUrl: 'http://159.89.242.197',
   authUrl: 'https://auth.tech-ascend.com',
   CourseUrl: 'https://tech-ascend.com/course-api',
   imagebucket: 'assets/images/'
